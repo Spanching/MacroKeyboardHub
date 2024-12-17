23bf3a9587577a19c97a797ff3013a9863a22048
Andreas Kruck <andreas.kruck@maibornwolff.de>
2024-12-17 10:28:36 +0100

    add pyinstaller configurations

---
+++ macro_keyboard_hub/pyinstaller.py
@@ -0,0 +1,14 @@
+import PyInstaller.__main__
+from pathlib import Path
+
+HERE = Path(__file__).parent.absolute()
+path_to_main = str(HERE / "__main__.py")
+
+def install():
+    PyInstaller.__main__.run([
+        path_to_main,
+        "--onefile",
+        "-w",
+        "-n gui"
+        # other pyinstaller options... 
+    ])
